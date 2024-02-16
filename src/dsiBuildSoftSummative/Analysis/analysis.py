16a4491f83f5b3991d5158e769e5e096dcb6c9f7
Gerald Chan <toronto@geraldchan.ca>
2024-02-16 01:23:00 -0500

    Issue 5 - Add code templte from instructor in analysis.py

--- src/dsiBuildSoftSummative/Analysis/analysis.py
+++ src/dsiBuildSoftSummative/Analysis/analysis.py
@@ -1,23 +1,36 @@
-import matplotlib
+from typing import Any, Optional
+import matplotlib.pyplot as plt
+import yaml
+import requests
 
-def Analysis():
 
-    def __init__(analysis_config:str):
-        # load config here
-        print('__init__')
-    
-    def load_data():
-        # load data here
-        print('load_data()')
+class Analysis():
 
-    def compute_analysis() -> dict:
-        # compute_analysis
-        print('compute_analysis()')
-    
-    def notify_done(message: str) -> None:
-        # notify user analysis is complete
-        print('notify_done()')
+    def __init__(self, analysis_config: str) -> None:
+        CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml']
 
-    def plot_data(save_path:Optional[str] = None) -> matplotlib.Figure:
-        # pot using matplotlib
-        print('plot_data()')
+        # add the analysis config to the list of paths to load
+        paths = CONFIG_PATHS + [analysis_config]
+
+        # initialize empty dictionary to hold the configuration
+        config = {}
+
+        # load each config file and update the config dictionary
+        for path in paths:
+            with open(path, 'r') as f:
+                this_config = yaml.safe_load(f)
+            config.update(this_config)
+
+        self.config = config
+
+    def load_data(self) -> None:
+        print(self.config['figure_title'])
+
+    def compute_analysis(self) -> Any:
+        pass
+
+    def plot_data(self, save_path: Optional[str] = None) -> plt.Figure:
+        pass
+
+    def notify_done(self, message: str) -> None:
+        pass
